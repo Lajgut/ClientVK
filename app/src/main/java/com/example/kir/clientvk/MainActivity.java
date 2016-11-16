371e198ea73816a8a6a1bb86e91c40960e673ed9
Kir <Kirbitkov@mail.ru>
2016-11-16 21:58:30 +0300

    Friends list

--- app/src/main/java/com/example/kir/clientvk/MainActivity.java
+++ app/src/main/java/com/example/kir/clientvk/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.kir.clientvk;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
+
+public class MainActivity extends Activity {
+
+    private String[] scope = new String[] {VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        //System.out.println(Arrays.asList(fingerprints));
+
+        VKSdk.login(this, scope);
+        lv = (ListView) findViewById(R.id.lv);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS,"first_name","last_name"));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+
+                        VKList list = (VKList) response.parsedModel;
+
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,list);
+
+                        lv.setAdapter(arrayAdapter);
+                    }
+                });
+                Toast.makeText(getApplicationContext(),"Good",Toast.LENGTH_LONG).show();
+
+            }
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext(),"LoginError",Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 }
